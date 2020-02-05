47f77763a51a74265535bf8c625224a5645448a5
Songkit Phachareon <serverx002@gmail.com>
2020-02-05 12:43:04 +0700

    Updates

---
+++ precache-manifest.c06ee7b8247173ca52593ca627ce2750.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eadd84f2fb7d3f72e4a5af600c1b085",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77f66f8885224645b5b2",
+    "url": "/static/css/2.e9609898.chunk.css"
+  },
+  {
+    "revision": "b3cbae5c67d4866b304e",
+    "url": "/static/css/main.1359096b.chunk.css"
+  },
+  {
+    "revision": "77f66f8885224645b5b2",
+    "url": "/static/js/2.c5bba9c8.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.c5bba9c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3cbae5c67d4866b304e",
+    "url": "/static/js/main.48790beb.chunk.js"
+  },
+  {
+    "revision": "ae54cd0cff8a67da8922",
+    "url": "/static/js/runtime-main.487ba98a.js"
+  }
+]);
